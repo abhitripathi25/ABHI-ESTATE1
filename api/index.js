fe84b097e6c943ff32c23c6c36afeb6fa8779a49
abhi250802 <abhiiit1234@gmail.com>
2023-12-21 06:57:23 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const app = express();
-
-app.listen(3000, () => {
+mongoose.connect("mongodb+srv://abhiiit1234:iitbombay1234@abhiestate1.emcnefi.mongodb.net/ABHIESTATE1?retryWrites=true&w=majority")
+.then(()=>{
+    console.log('connected to mongodb');
+}).catch((err)=>{
+    console.log(err); 
+})
+const app = express(); 
+  
+app.listen(3000, () => {   
     console.log('server is running at port 3000');
 })
