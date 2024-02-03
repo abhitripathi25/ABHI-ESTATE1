3a8bc957a75a2a57b7bdf247f0e279c1fe665180
abhi250802 <abhiiit1234@gmail.com>
2024-02-03 23:14:44 +0530

    complete signin page func

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
-
+import {errorHandler} from '../utils/error.js'
+import  jwt  from "jsonwebtoken";
 // yha hume user ke trf se jo milega wo bhejenge db me
 export const signup = async(req,res,next) => {
     const {username, email, password} = req.body;
@@ -13,3 +14,25 @@ export const signup = async(req,res,next) => {
         next(error)
     }
  }
+
+ export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+      const validUser = await User.findOne({ email });
+  
+      if (!validUser) {
+        return next(errorHandler(404, 'User not found'));
+      }
+      const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) {
+            return next(errorHandler(401, 'wrong credentials'));
+        }
+        const token = jwt.sign({id: validUser._id}, 
+            process.env.JWT_SECRET);
+            const {password: pass, ...rest}= validUser._doc;
+            res.cookie('access_token', token, 
+            { httpOnly: true}).status(200).json(rest); 
+    } catch (error) {
+      next(error);
+    }
+  };
